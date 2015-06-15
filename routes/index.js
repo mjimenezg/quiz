bbb203821ccac447c847f7a7e78f780c8f079b5d
mjimenezg <mjimenez@unizar.es>
2015-06-15 14:02:59 +0200

    Mod6-P2P añadir página de créditos

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,9 @@ var quizController = require('../controllers/quiz_controller');
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Créditos' });
+});
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer',   quizController.answer);

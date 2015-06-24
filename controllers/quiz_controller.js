018d5749a9b2d049dd90fe7de454fbeba6f04480
mjimenezg <mjimenez@unizar.es>
2015-06-24 17:55:07 +0200

    Mod8-P2P Añadir tema

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -65,7 +65,7 @@ exports.create = function(req, res) {
         res.render('quizes/new', {quiz: quiz, errors: err.errors});
       } else {
         quiz // save: guarda en DB campos pregunta y respuesta de quiz
-        .save({fields: ["pregunta", "respuesta"]})
+        .save({fields: ["pregunta", "respuesta", "tema"]})
         .then( function(){ res.redirect('/quizes')})
       }      // res.redirect: Redirección HTTP a lista de preguntas
     }
@@ -83,6 +83,7 @@ exports.edit = function(req, res) {
 exports.update = function(req, res) {
   req.quiz.pregunta  = req.body.quiz.pregunta;
   req.quiz.respuesta = req.body.quiz.respuesta;
+  req.quiz.tema = req.body.quiz.tema;
 
   req.quiz
   .validate()
@@ -92,7 +93,7 @@ exports.update = function(req, res) {
         res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
       } else {
         req.quiz     // save: guarda campos pregunta y respuesta en DB
-        .save( {fields: ["pregunta", "respuesta"]})
+        .save( {fields: ["pregunta", "respuesta", "tema"]})
         .then( function(){ res.redirect('/quizes');});
       }     // Redirección HTTP a lista de preguntas (URL relativo)
     }
--- models/quiz.js
+++ models/quiz.js
@@ -6,7 +6,9 @@ module.exports = function(sequelize, DataTypes) {
     { pregunta:  { type: DataTypes.STRING,
                    validate: { notEmpty: {msg: "-> Falta Pregunta"}}  },
       respuesta: { type: DataTypes.STRING,
-                   validate: { notEmpty: {msg: "-> Falta Respuesta"}} }
+                   validate: { notEmpty: {msg: "-> Falta Respuesta"}} },
+      tema: { type: DataTypes.STRING,
+              validate: { notEmpty: {msg: "-> Falta tema"}} }
     }
   );
 }
